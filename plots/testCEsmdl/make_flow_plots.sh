1f723d2fb53b310fea362425fe88429d29c17499
Storm Colloms <2297403c@wiay.astro.gla.ac.uk>
2024-11-19 12:30:05 +0000

    run with CE test model removed

---
+++ plots/testCEsmdl/make_flow_plots.sh
@@ -0,0 +1,8 @@
+flow_path="/data/wiay/2297403c/amaze_model_select/Nflows_AMAZE_paper/inputs/flow_models/CE_test_population/"
+
+python /data/wiay/2297403c/amaze_model_select/AMAZE_model_selection/plot/flow_results/make_paper_plots.py \
+    --flow-path ${flow_path} \
+    --channel-label 'CE'\
+    --hyperparam-idxs 1 2 \
+    --conditional 0.1 1.0 \
+    --outdir '/data/wiay/2297403c/amaze_model_select/Nflows_AMAZE_paper/plots/testCEsmdl' \
