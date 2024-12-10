bcb29df6c1d8f125199100ee2051cf1282b4ee41
Storm Colloms <2297403c@wiay.astro.gla.ac.uk>
2024-12-10 13:53:45 +0000

    updated results plots

--- plots/prod_091224/make_flow_plots.sh
+++ plots/prod_091224/make_flow_plots.sh
@@ -1,15 +1,14 @@
+#!/bin/bash
 flow_path="/data/wiay/2297403c/amaze_model_select/Nflows_AMAZE_paper/inputs/flow_models/mixed_models/"
 
-python /data/wiay/2297403c/amaze_model_select/AMAZE_model_selection/plot/flow_results/make_paper_plots.py \
+/data/wiay/2297403c/conda_envs/amaze/bin/python /data/wiay/2297403c/amaze_model_select/AMAZE_model_selection/plot/flow_results/make_paper_plots.py \
     --flow-path ${flow_path} \
     --channel-label 'CE' 'CHE' 'GC' 'NSC' 'SMT' \
     --hyperparam-idxs 0 2 \
     --conditional 0.0 1.0 \
-    --plot-flow-corner \
     --plot-discrete-result \
-    --plot-cont-result \
-    --save-det-betas \
-    --cont-result-path '/data/wiay/2297403c/amaze_model_select/Nflows_AMAZE_paper/outputs/cont_GWTC3/prod_retrainedCE/' \
-    --discrete-result-path '/data/wiay/2297403c/amaze_model_select/Nflows_AMAZE_paper/outputs/discrete_GWTC3/flow_retrainedCE/' \
+    --justplot \
     --KDE-result-path '/data/wiay/2297403c/amaze_model_select/Nflows_AMAZE_paper/outputs/discrete_GWTC3/KDEs/' \
+    --discrete-result-path '/data/wiay/2297403c/amaze_model_select/Nflows_AMAZE_paper/outputs/discrete_GWTC3/flow_retrainedCE/' \
+    --cont-result-path '/data/wiay/2297403c/amaze_model_select/Nflows_AMAZE_paper/outputs/cont_GWTC3/prod_retrainedCE/' \
     --outdir '/data/wiay/2297403c/amaze_model_select/Nflows_AMAZE_paper/plots/prod_091224' \
